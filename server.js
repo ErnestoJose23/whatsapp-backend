9f51011dafe1c612fcfb47e141c5b3e65b34b523
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-15 12:42:08 +0200

    server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+const connection_url =
+  "mongodb+srv://admin:fUi8PFzCUgWMRCp@cluster0.3ni6y.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  userCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => res.status(200).send("hello wrld"));
 
